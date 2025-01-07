cbdb921f79543de03e67666fb841e7572d633eb5
Ivy Tanui <ivy.tanui@strathmore.edu>
2025-01-07 19:42:20 +0300

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin; // Ensure you export the function if you are running tests
+  
